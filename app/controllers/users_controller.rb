083fbbecc85af55facb2835ebe975351266717ee
ALEX SLYE <slyealex@live.com>
2016-07-25 19:26:32 -0400

    add methods to User controller, create, new, show, user_params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: "successfully deleted account!"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :location)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
   root 'homes#show'
 
   resources :homes

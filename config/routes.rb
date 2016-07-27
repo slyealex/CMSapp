73d2387bd76fe1d3c16681714f37fffa9133a7db
ALEX SLYE <slyealex@live.com>
2016-07-27 13:44:22 -0400

    add content form and corisponding index show and create views

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'contents/show'
 
-  get 'contents/new'
-
-  get 'contents/index'
-
-  get 'content/show'
-
-  get 'content/index'
-
-  root 'homes#show'
+  root 'contents#index'
 
   resources :homes
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :contents
 
 end

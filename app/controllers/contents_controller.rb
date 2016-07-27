95ade95d4a2c5abe2414054f2da40456ca916327
ALEX SLYE <slyealex@live.com>
2016-07-27 13:16:59 -0400

    added basic CRUD methods for contents controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,10 +1,29 @@
 class ContentsController < ApplicationController
   def show
+
   end
 
   def new
+    @content = Content.new
   end
 
   def index
+    @contents = Content.all
+  end
+
+  def create
+    @content = Content.new(content_params)
+
+    if @content.save
+      redirect_to root_path, notice: "Content created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :content, :url)
   end
 end
